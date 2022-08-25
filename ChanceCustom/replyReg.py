51d73876b2da2ef817379d8221e27f8cfb632055
Fitz161 <Fitz1611@outlook.com>
2022-08-25 21:39:41 +0800

    新增六个算法计算变量

--- ChanceCustom/replyReg.py
+++ ChanceCustom/replyReg.py
@@ -121,7 +121,13 @@ listRegTotalFun = [
     ['RD', ['表达式'], ChanceCustom.replyOlivaDice.RDFunTemp()],
 
     # >算法计算<
-    ['计算', ['计算公式'], ChanceCustom.replyEval.evalExprFunTemp()]
+    ['计算', ['计算公式'], ChanceCustom.replyEval.evalExprFunTemp()],
+    ['分割排序', ['排序文本', '分割符号', '依据序号', '排序正逆'], ChanceCustom.replyEval.splitSortFunTemp(type="sort")],
+    ['随机排序', ['排序文本', '分割符号'], ChanceCustom.replyEval.splitSortFunTemp(type="shuffle")],
+    ['统计', ['被统计文本', '统计出现的文本'], ChanceCustom.replyEval.wordCountFunTemp()],
+    ['取MD5', ['被取目标', 'MD5位数'], ChanceCustom.replyEval.getMD5FunTemp()],
+    ['进制', ['待转化数值', '原数值进制', '目标进制'], ChanceCustom.replyEval.baseConvFunTmp()],
+    ['补位', ['待补位文本', '结果长度', '补位字符', '结尾/开头'], ChanceCustom.replyEval.charPaddingFunTemp()]
 ]
 
 listRegTotal = [
