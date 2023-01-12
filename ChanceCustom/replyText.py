598546c9dc75997037293f5da07249673371d812
Fitz161 <Fitz1611@outlook.com>
2023-01-12 10:47:01 +0800

    新增高级文本操作变量 (#21)
    * 新增高级文本操作变量
    
    * 修正转义优先级
    
    Co-authored-by: 仑质 <benzenpenxil@foxmail.com>

--- ChanceCustom/replyText.py
+++ ChanceCustom/replyText.py
@@ -485,3 +485,287 @@ def replaceTextForEachLineFunTemp():
             return res
         return replaceTextForEachLine_f
     return replaceTextForEachLineFun
+
+def hideResultFunTemp():
+    def hideResultFun(valDict):
+        def hideResult_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            return res
+        return hideResult_f
+    return hideResultFun
+
+def splitAndSelectFunTemp():
+    def splitAndSelectFun(valDict):
+        def splitAndSelect_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '被分割文本', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '分隔符', '\n', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '取出序号', '0', groupDict, valDict)
+            delimiter = resDict['分隔符']
+            splited_text = resDict['被分割文本'].split(delimiter)
+            selectType = str2int(resDict['取出序号'])
+            if selectType == 0:
+                res = random.choice(splited_text)
+            elif selectType > 0:
+                res = splited_text[min(selectType, len(splited_text)) - 1]
+            else:
+                res = splited_text[max(selectType, -len(splited_text))]
+            return res
+        return splitAndSelect_f
+    return splitAndSelectFun
+
+def roundNumberFunTemp():
+    def str2float(floatStr, default=.0):
+        try:
+            return float(floatStr)
+        except:
+            return default
+    def roundNumberFun(valDict):
+        def roundNumber_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '数值', '0', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '下界', None, groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '上界', None, groupDict, valDict)
+            upperBound = str2float(resDict['上界'], float('inf'))
+            lowerBound = str2float(resDict['下界'], -float('inf'))
+            val = max(min(str2float(resDict['数值']), upperBound), lowerBound)
+            res = str(int(val)) if val == int(val) else str(val)
+            return res
+        return roundNumber_f
+    return roundNumberFun
+
+def filterNumberFunTemp():
+    def filterNumberFun(valDict):
+        def filterNumber_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '被取文本', '', groupDict, valDict)
+            numbers = []
+            for c in resDict['被取文本']:
+                if c.isdigit():
+                    numbers.append(c)
+            res = ''.join(numbers)
+            return res
+        return filterNumber_f
+    return filterNumberFun
+
+def selectMiddleFunTemp():
+    def selectMiddleFun(valDict):
+        def selectMiddle_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            
+            ChanceCustom.replyBase.getCharRegTotal(resDict, 'XX1', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, 'XX2', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, 'XX3', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, 'XX4', '', groupDict, valDict)
+            text = resDict['XX1']
+            leftText = resDict['XX2']
+            rightText = resDict['XX3']
+            selectType = resDict['XX4']
+
+            pattern = leftText + '(.*?)' + rightText
+            candidates = re.findall(pattern, text)
+
+            if selectType == '':
+                if candidates is []:
+                    candidates.append('')
+                res = random.choice(candidates)
+            elif selectType.isdigit():
+                res = candidates[min(len(candidates), str2int(selectType)) - 1]
+            elif selectType.startswith('all'):
+                delimeter = selectType[3:]
+                if delimeter is '':
+                    for i, candidate in enumerate(candidates):
+                        res += f'{i+1}.{candidate} '
+                    res = res[:-1]
+                else:
+                    res = delimeter.join(candidates)
+            elif selectType.startswith('top'):
+                limit = str2int(selectType[3:], 1)
+                res = ' '.join(candidates[:limit])
+            elif selectType.startswith('bot'):
+                limit = str2int(selectType[3:], 1)
+                res = ' '.join(candidates[-limit:]) 
+            return res
+        return selectMiddle_f
+    return selectMiddleFun
+
+def regExpRefactorFunTemp():
+    def regExpRefactorFun(valDict):
+        def regExpRefactor_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '重构文本', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '表达式', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '重构格式', '', groupDict, valDict)
+            pattern = resDict['表达式']
+            text = resDict['重构文本']
+            match = re.search(pattern, text)
+
+            if match is None:
+                res = ''
+            elif resDict['重构格式'].startswith('匹配') and len(resDict['重构格式']) > 2:
+                selectType = str2int(resDict['重构格式'][2:]) - 1
+                sub_matches = re.search(pattern, text).groups()
+                if 0 < selectType < len(sub_matches) + 1:
+                    res = sub_matches[selectType - 1]
+            else:
+                res = match.group()
+            return res
+        return regExpRefactor_f
+    return regExpRefactorFun
+
+def regExpReplaceFunTemp():
+    def regExpReplaceFun(valDict):
+        def regExpReplace_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '表达式', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '被搜索的文本', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '序次', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '替换文本', None, groupDict, valDict)
+            text = resDict['被搜索的文本']
+            pattern = re.compile(resDict['表达式'])
+            matched_items = pattern.findall(text)
+            new_exp = resDict['替换文本']
+            selectType = resDict['序次']
+
+            if new_exp is None: 
+                if len(matched_items) == 0:
+                    res = ''
+                elif selectType == '':
+                    res = random.choice(matched_items)
+                elif selectType.isdigit():
+                    res = matched_items[min(len(matched_items), str2int(selectType)) - 1]
+                elif selectType.startswith('all'):
+                    delimeter = selectType[3:]
+                    if delimeter is '':
+                        for i, item in enumerate(matched_items):
+                            res += f'{i+1}.{item} '
+                        res = res[:-1]
+                    else:
+                        res = delimeter.join(matched_items)
+                elif selectType.startswith('top'):
+                    limit = str2int(selectType[3:], 1)
+                    res = ' '.join(matched_items[:limit])
+                elif selectType.startswith('bot'):
+                    limit = str2int(selectType[3:], 1)
+                    res = ' '.join(matched_items[-limit:])
+            else:
+                res = re.sub(pattern, new_exp, text, count=max(0, str2int(resDict['序次'])))
+            return res
+        return regExpReplace_f
+    return regExpReplaceFun
+
+def subRegExpFunTemp():
+    def subRegExpFun(valDict):
+        def subRegExp_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '表达式', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '被搜索的文本', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '序次', '1', groupDict, valDict)
+            text = resDict['被搜索的文本']
+            pattern = re.compile(resDict['表达式'])
+            match = re.search(pattern, text)
+            selectType = str2int(resDict['序次'])
+
+            if match is None:
+                res = ''
+            elif selectType == 0:
+                res = match.group()
+            else:
+                sub_matches = match.groups()
+                if 0 < selectType < len(sub_matches) + 1:
+                    res = sub_matches[selectType - 1]
+                else:
+                    res = sub_matches[0]
+            return res
+        return subRegExp_f
+    return subRegExpFun
+
+def deleteTextFunTemp():
+    def deleteTextFun(valDict):
+        def deleteText_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '欲被删除文本', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '删除文本开头', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '删除文本结尾', '', groupDict, valDict)
+            text = resDict['欲被删除文本']
+            pattern = re.compile(resDict['删除文本开头'] + '.*?' + resDict['删除文本结尾'])
+            res = re.sub(pattern, '', text, count=0)
+            return res
+        return deleteText_f
+    return deleteTextFun
+
+def switchAndReturnFunTemp():
+    def switchAndReturnFun(valDict):
+        def switchAndReturn_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '序号或逻辑组', None, groupDict, valDict)
+            ChanceCustom.replyBase.getDataRaw(resDict, '...', None, groupDict)
+            case = resDict['序号或逻辑组']
+            cases = resDict['...']
+            if None == case or None == cases or 0 == len(cases):
+                res = ''
+            elif case.find('||') != -1:
+                logic_exps = case.split('||')
+                flag = False
+                for i, logic_exp in enumerate(logic_exps):
+                    if logic_exp == '真':
+                        res = cases[i] if len(cases) > i else ''
+                        flag = True
+                        break
+                if not flag:
+                    res = cases[-1] if len(cases) > 0 else ''
+            else:
+                res = cases[max(min(str2int(case) - 1, len(cases)), 0)]
+            return res
+        return switchAndReturn_f
+    return switchAndReturnFun
+
+def replaceFunTemp():
+    def replaceFun(valDict):
+        def replace_f(matched:'re.Match|dict'):
+            groupDict = ChanceCustom.replyBase.getGroupDictInit(matched)
+            res = ''
+            resDict = {}
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '被替换内容', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '被替换文本', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '替换文本', '', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '替换次数', '-1', groupDict, valDict)
+            ChanceCustom.replyBase.getCharRegTotal(resDict, '并列替换', '', groupDict, valDict)
+            text = resDict['被替换内容']
+            count = str2int(resDict['替换次数'])
+            pattern = resDict['被替换文本']
+            repl = resDict['替换文本']
+            if resDict['并列替换'] != '[并列]':
+                res = text.replace(pattern, repl, count)
+            else:
+                patterns = pattern.split('||')
+                replaces = repl.split('||')
+                if len(patterns) != len(replaces):
+                    res = text.replace(pattern, repl, count)
+                else:
+                    for pattern, repl in zip(patterns, replaces):
+                        text = text.replace(pattern, repl, count)
+                    res = text
+            return res
+        return replace_f
+    return replaceFun
